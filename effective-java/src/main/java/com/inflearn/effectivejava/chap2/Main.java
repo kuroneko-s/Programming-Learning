6f61a8a768939a4aed645dff21aece6bf321d8e3
kuroneko <drivespublic@gmail.com>
2023-08-29 19:23:21 +0900

    chap2. Heap Pollution(힙오염) 간단 테스트.

--- effective-java/src/main/java/com/inflearn/effectivejava/chap2/Main.java
+++ effective-java/src/main/java/com/inflearn/effectivejava/chap2/Main.java
@@ -4,6 +4,9 @@ import org.springframework.boot.jdbc.UnsupportedDataSourcePropertyException;
 import org.springframework.boot.jdbc.metadata.HikariDataSourcePoolMetadata;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +22,20 @@ public class Main {
                 .addCustom("q-94u1")
                 .build();
         System.out.println(customPizza);
+
+
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("123");
+        Object obj = list1;
+        ArrayList<Integer> list2 = (ArrayList<Integer>)obj;
+        list2.add(123);
+
+        List<String> list3 = Collections.checkedList(new ArrayList<>(), String.class);
+        list3.add("123");
+
+        Object obj2 = list3;
+        List<Integer> list4 = Collections.checkedList((List<Integer>) obj2, Integer.class);
+        // list4.add(123); ClassCastException
 
 
     }
