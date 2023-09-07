8b564a6d4b2fcc82be9c21c67d105e4bb46cae99
donghyuk <drivespublic@gmail.com>
2023-09-08 08:15:10 +0900

    chap6 -F

--- effective-java/src/main/java/com/inflearn/effectivejava/chap6/Main.java
+++ effective-java/src/main/java/com/inflearn/effectivejava/chap6/Main.java
@@ -1,5 +1,8 @@
 package com.inflearn.effectivejava.chap6;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -19,6 +22,18 @@ public class Main {
         Pattern pattern = Pattern.compile("test");
         String test = pattern.matcher("test31r1r321howqfhitesthiowthoi31").replaceAll("12345");
         System.out.println(test);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "test");
+        map.put("key2", "test");
+        map.put("key3", "test");
+        map.put("key4", "test");
+        map.put("key5", "test");
+
+        Set<String> keySet1 = map.keySet();
+        Set<String> keySet2 = map.keySet();
+
+        System.out.println(keySet1 == keySet2);
     }
 }
 
