7b72a410f405084f582dfa4e05a0766721ec0af4
최동혁 <drivespublic@gmail.com>
2023-10-06 20:04:58 +0900

    chap10. 진행중

--- effective-java/src/main/java/com/inflearn/effectivejava/chap10/Main.java
+++ effective-java/src/main/java/com/inflearn/effectivejava/chap10/Main.java
@@ -8,23 +8,23 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws MalformedURLException {
         // broke in java code
-//        long time = System.currentTimeMillis();
-//        Timestamp timestamp = new Timestamp(time);
-//        Date date = new Date(time);
-//
-//        System.out.println(timestamp instanceof Date);
-//        System.out.println(date.equals(timestamp));
-//        System.out.println(timestamp.equals(date));
-//
-//        URL google1 = new URL("https", "about.google", "/products");
-//        URL google2 = new URL("https", "about.google", "/products");
-//        System.out.println(google1.equals(google2));
-//
-//        Objects.equals(null, null);
+        long time = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(time);
+        Date date = new Date(time);
 
-//        List<Point> points = new ArrayList<>();
-//        points.add(new Point());
-//        System.out.println(points.contains(new Point()));
+        System.out.println(timestamp instanceof Date);
+        System.out.println(date.equals(timestamp));
+        System.out.println(timestamp.equals(date));
+
+        URL google1 = new URL("https", "about.google", "/products");
+        URL google2 = new URL("https", "about.google", "/products");
+        System.out.println(google1.equals(google2));
+
+        Objects.equals(null, null);
+
+        List<Point> points = new ArrayList<>();
+        points.add(new Point());
+        System.out.println(points.contains(new Point()));
 
         System.out.println(UUID.randomUUID());
     }
